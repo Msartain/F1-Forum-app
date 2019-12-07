d596d54d270f19b63fe0bf94cdf42e3bdcf7ae9d
Matthew Sartain <matthewsartain@me.com>
2019-12-06 23:25:08 -0600

    go back button added, user profile picture added, dropdown for log out added

--- public/javascripts/edit-post.js
+++ public/javascripts/edit-post.js
@@ -1,11 +1,25 @@
-let edit = document.getElementById('edit')
-let show = document.getElementById('show')
-// let textArea = document.getElementById('input')
-let form = document.getElementById('form')
+let edit = document.getElementById('edit');
+let show = document.getElementById('show');
+let form = document.getElementById('form');
+let back = document.getElementById('back');
+let del = document.getElementById('del');
 
 edit.addEventListener('click', function(evt){
     evt.preventDefault()
-    edit.classList = 'hide';
     form.classList = '';
     show.classList = 'hide';    
+})
+
+back.addEventListener('click', function(evt){
+    evt.preventDefault();
+    form.classList = 'hide';
+    show.classList = 'card container px-0 mt-4';
+})
+
+del.addEventListener('click', function(evt){
+    console.log('heard the click!!!')
+    let delPost = confirm("Are you sure you want to delete?");
+    alert( delPost );
+//     evt.preventDefault()
+//    evt.Window.confirm("Are you sure you want to delete?")
 })
